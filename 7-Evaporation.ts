3e8643716516c98c71677c04ec29a0453c96763e
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-05-30 16:22:12 +0100

    removes comments

--- 7-Evaporation.ts
+++ 7-Evaporation.ts
@@ -4,17 +4,6 @@ interface evaporationConstants {
     threshold: number
 }
 
-// export function evaporator(content: number, evapPerDay: number, threshold: number): number {
-//     const thresholdInml: number = content * (threshold / 100);
-//     return thresholdInml;
-//     let days: number = 1
-//     while(content > thresholdInml){
-//       days += 1
-//       content = content * (evapPerDay /100)
-//     }
-//     return days;
-//   }
-
 export const evaporator = ({content, evapPerDay, threshold}: evaporationConstants): number => {
     const thresholdInml: number = content * (threshold / 100);
     let days: number = 1;
