1ef7633c82b37da56d7fd42d51cb925950c84d60
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-08-05 14:49:13 +0100

    solves 100 prisoners

--- js-problems/6-100-prisoners.js
+++ js-problems/6-100-prisoners.js
@@ -1,110 +1,155 @@
-const solve100Prisoners = context => {
+// const solve100Prisoners = context => {
   
-};
+// };
 
-const _ = require('lodash');
+// const _ = require('lodash');
  
-const numPlays = 100000;
+// const numPlays = 100000;
  
-const setupSecrets = () => {
-	// setup the drawers with random cards
-	let secrets = [];
+// const setupSecrets = () => {
+// 	// setup the drawers with random cards
+// 	let secrets = [];
  
-	for (let i = 0; i < 100; i++) {
-		secrets.push(i);
-	}
+// 	for (let i = 0; i < 100; i++) {
+// 		secrets.push(i);
+// 	}
  
-	return _.shuffle(secrets);
-}
+// 	return _.shuffle(secrets);
+// }
  
-// const playOptimal = () => {
+// // const playOptimal = () => {
+ 
+// // 	let secrets = setupSecrets();
+ 
+ 
+// // 	// Iterate once per prisoner
+// // 	loop1:
+// // 	for (let p = 0; p < 100; p++) {
+ 
+// // 		// whether the prisoner succeedss
+// // 		let success = false;
+ 
+// // 		// the drawer number the prisoner chose
+// // 		let choice = p;
+ 
+ 
+// // 		// The prisoner can choose up to 50 cards
+// // 		loop2:
+// // 		for (let i = 0; i < 50; i++) {
+ 
+// // 			// if the card in the drawer that the prisoner chose is his card
+// // 			if (secrets[choice] === p){
+// // 				success = true;
+// // 				break loop2;
+// // 			}
+ 
+// // 			// the next drawer the prisoner chooses will be the number of the card he has.
+// // 			choice = secrets[choice];
+ 
+// // 		}	// each prisoner gets 50 chances
+ 
+ 
+// // 		if (!success) return false;
+ 
+// // 	} // iterate for each prisoner 
+ 
+// // 	return true;
+// // }
+ 
+// const playRandom = () => {
  
 // 	let secrets = setupSecrets();
  
- 
-// 	// Iterate once per prisoner
-// 	loop1:
+// 	// iterate for each prisoner 
 // 	for (let p = 0; p < 100; p++) {
  
-// 		// whether the prisoner succeedss
+// 		let choices = setupSecrets();
+ 
 // 		let success = false;
  
-// 		// the drawer number the prisoner chose
-// 		let choice = p;
- 
- 
-// 		// The prisoner can choose up to 50 cards
-// 		loop2:
 // 		for (let i = 0; i < 50; i++) {
  
-// 			// if the card in the drawer that the prisoner chose is his card
-// 			if (secrets[choice] === p){
+// 			if (choices[i] === p) {
 // 				success = true;
-// 				break loop2;
+// 				break;
 // 			}
- 
-// 			// the next drawer the prisoner chooses will be the number of the card he has.
-// 			choice = secrets[choice];
- 
-// 		}	// each prisoner gets 50 chances
- 
+// 		}
  
 // 		if (!success) return false;
- 
-// 	} // iterate for each prisoner 
+// 	}
  
 // 	return true;
 // }
  
-const playRandom = () => {
+// // const execOptimal = () => {
  
-	let secrets = setupSecrets();
+// // 	let success = 0;
  
-	// iterate for each prisoner 
-	for (let p = 0; p < 100; p++) {
+// // 	for (let i = 0; i < numPlays; i++) {
  
-		let choices = setupSecrets();
+// // 		if (playOptimal()) success++;
  
-		let success = false;
+// // 	}
  
-		for (let i = 0; i < 50; i++) {
+// // 	return 100.0 * success / 100000;
+// // }
  
-			if (choices[i] === p) {
-				success = true;
-				break;
-			}
-		}
- 
-		if (!success) return false;
-	}
- 
-	return true;
-}
- 
-// const execOptimal = () => {
+// const execRandom = () => {
  
 // 	let success = 0;
  
 // 	for (let i = 0; i < numPlays; i++) {
  
-// 		if (playOptimal()) success++;
+// 		if (playRandom()) success++;
  
 // 	}
  
 // 	return 100.0 * success / 100000;
 // }
- 
-const execRandom = () => {
- 
-	let success = 0;
- 
-	for (let i = 0; i < numPlays; i++) {
- 
-		if (playRandom()) success++;
- 
-	}
- 
-	return 100.0 * success / 100000;
-}
 
-console.log(execRandom());
+// console.log(execRandom());
+
+const solve100Prisoners = context => {
+	// based on the explanation from the video
+	while (!context.isProcessComplete()) {
+	  const prisoner = context.getCurrentPrisoner();
+	  let slip = prisoner
+	  // prisoner tries at 50 drawers
+	  for (let i = 0; i < 50; i++) {
+		const drawer = context.openDrawer(slip);
+		if (!drawer) {
+		  return;
+		}
+		if (drawer.success) {
+		  // prisoner found the needed drawer no need to search further
+		  break;
+		}
+		slip = drawer.slipLabel
+	  } 
+	
+	}
+	};
+	
+	// const solve100Prisoners = context => {
+	//   const { getCurrentPrisoner, openDrawer, isProcessComplete } = context
+	  
+	//   while (!isProcessComplete()) {
+	// 	let label = getCurrentPrisoner()
+		
+	// 	for (let i = 0; i < 50; i++) {
+	// 	  const result = openDrawer(label)
+		
+	// 	  if (!result) {
+	// 		break;
+	// 	  }
+		
+	// 	  const { success, slipLabel } = result
+		
+	// 	  if (success) {
+	// 		break;
+	// 	  } else {
+	// 		label = slipLabel
+	// 	  }
+	// 	}
+	//   }
+	// };
