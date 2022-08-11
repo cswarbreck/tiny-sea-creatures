f32796fb47137773c826e810b5991bce20bf0e57
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-08-11 21:06:07 +0100

    Refactor

--- js-problems/5-quick-sort.js
+++ js-problems/5-quick-sort.js
@@ -1,42 +1,47 @@
-var items = [5,3,7,6,2,9];
-function swap(items, leftIndex, rightIndex){
-    var temp = items[leftIndex];
+const items = [5,3,7,6,2,9,12,64,23,12,43,25,78,12,];
+
+swap = (items, leftIndex, rightIndex) => {
+    let temp = items[leftIndex];
     items[leftIndex] = items[rightIndex];
     items[rightIndex] = temp;
 }
-function partition(items, left, right) {
-    var pivot   = items[Math.floor((right + left) / 2)], //middle element
-        i       = left, //left pointer
-        j       = right; //right pointer
-    while (i <= j) {
-        while (items[i] < pivot) {
-            i++;
-        }
-        while (items[j] > pivot) {
-            j--;
-        }
-        if (i <= j) {
-            swap(items, i, j); //sawpping two elements
-            i++;
-            j--;
-        }
-    }
-    return i;
-}
 
-function quickSort(items, left, right) {
-    var index;
+ partition = (items, left, right) => {
+        let pivot = items[Math.floor((right + left) / 2)], //middle element
+            i = left, //left pointer
+            j = right; //right pointer
+        while (i <= j) {
+            while (items[i] < pivot) {
+                i++;
+            }
+            while (items[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                swap(items, i, j); //sawpping two elements
+                i++;
+                j--;
+            }
+        }
+        return i;
+    }
+
+quickSort = (items, left, right) => {
+    let index;
     if (items.length > 1) {
-        index = partition(items, left, right); //index returned from partition
-        if (left < index - 1) { //more elements on the left side of the pivot
+        //index returned from partition
+        index = partition(items, left, right); 
+        if (left < index - 1) { 
+            //more elements on the left side of the pivot
             quickSort(items, left, index - 1);
         }
-        if (index < right) { //more elements on the right side of the pivot
+        if (index < right) { 
+            //more elements on the right side of the pivot
             quickSort(items, index, right);
         }
     }
     return items;
 }
 // first call to quick sort
-var sortedArray = quickSort(items, 0, items.length - 1);
+let sortedArray = quickSort(items, 0, items.length - 1);
 console.log(sortedArray); //prints [2,3,5,6,7,9]
