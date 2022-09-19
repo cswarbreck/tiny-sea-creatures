a36e455ef4c435a51c2ee78e8f936304fd3e9138
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-09-19 19:15:04 +0100

    compiled

---
+++ sorting-algorithms/binary-search.js
@@ -0,0 +1,14 @@
+var binarySearch = function (nums, target) {
+    var left = 0;
+    var right = nums.length - 1;
+    while (left <= right) {
+        var mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target)
+            return mid;
+        if (target < nums[mid])
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    return -1;
+};
