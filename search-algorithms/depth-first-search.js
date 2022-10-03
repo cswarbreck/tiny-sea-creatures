e4786bd611cb7cf6101e0423d694177d41399217
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-10-03 19:36:44 +0100

    DFS

---
+++ search-algorithms/depth-first-search.js
@@ -0,0 +1,24 @@
+"use strict";
+exports.__esModule = true;
+var graph_1 = require("./graph");
+function dfs(start, finish, visited) {
+    if (visited === void 0) { visited = new Set(); }
+    console.log(start);
+    visited.add(start);
+    var destinations = graph_1["default"].get(start);
+    var routeFound = false;
+    for (var _i = 0, destinations_1 = destinations; _i < destinations_1.length; _i++) {
+        var destination = destinations_1[_i];
+        if (destination === 'BKK') {
+            routeFound = true;
+            return console.log("There is a route from ".concat(start, " to ").concat(finish));
+        }
+        if (!visited.has(destination)) {
+            dfs(destination, visited);
+        }
+    }
+    if (!routeFound) {
+        console.log('There is no route. Would you like to try a different destination?');
+    }
+}
+dfs('PHX', 'BKK');
