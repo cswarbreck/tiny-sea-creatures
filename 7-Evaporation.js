d691205f822b2f0b96b44d204b4f90dcb59366be
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-11-18 18:26:51 +0000

    removes comments

--- 7-Evaporation.js
+++ 7-Evaporation.js
@@ -1,30 +1,15 @@
-"use strict";
-exports.__esModule = true;
-exports.evaporator = void 0;
-// export function evaporator(content: number, evapPerDay: number, threshold: number): number {
-//     const thresholdInml: number = content * (threshold / 100);
-//     return thresholdInml;
-//     let days: number = 1
-//     while(content > thresholdInml){
-//       days += 1
-//       content = content * (evapPerDay /100)
-//     }
-//     return days;
-//   }
-var evaporator = function (_a) {
-    var content = _a.content, evapPerDay = _a.evapPerDay, threshold = _a.threshold;
-    var thresholdInml = content * (threshold / 100);
-    var days = 1;
+export const evaporator = ({ content, evapPerDay, threshold }) => {
+    const thresholdInml = content * (threshold / 100);
+    let days = 1;
     while (content > thresholdInml) {
         days += 1;
         content = content * (evapPerDay / 100);
     }
     return days;
 };
-exports.evaporator = evaporator;
-var demoRates = {
+const demoRates = {
     content: 10,
     evapPerDay: 10,
     threshold: 10
 };
-console.log((0, exports.evaporator)(demoRates));
+console.log(evaporator(demoRates));
