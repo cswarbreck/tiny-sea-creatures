de785d1afd054c97e6b60103a2969d11250eb0a3
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-11-18 18:26:39 +0000

    es6

--- 6-sum-of-digits.js
+++ 6-sum-of-digits.js
@@ -1,17 +1,13 @@
-"use strict";
-exports.__esModule = true;
-exports.digitalRoot = void 0;
-var digitalRoot = function (n) {
+export const digitalRoot = (n) => {
     if (n < 10) {
         return n;
     }
     else {
-        var stringed = n.toString().split('');
-        var resultArray_1 = [];
-        stringed.forEach(function (el) { resultArray_1.push(parseInt(el)); });
-        var result = resultArray_1.reduce(function (a, b) { return a + b; });
-        return (0, exports.digitalRoot)(result);
+        let stringed = n.toString().split('');
+        let resultArray = [];
+        stringed.forEach((el) => { resultArray.push(parseInt(el)); });
+        const result = resultArray.reduce((a, b) => { return a + b; });
+        return digitalRoot(result);
     }
 };
-exports.digitalRoot = digitalRoot;
-console.log((0, exports.digitalRoot)(147));
+console.log(digitalRoot(147));
