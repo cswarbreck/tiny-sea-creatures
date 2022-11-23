3e326f27b3b93ee9d17e31b602805a78a6a50af4
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-11-23 21:57:17 +0000

    graph vertex

--- data-structures/graphVertex.js
+++ data-structures/graphVertex.js
@@ -1,19 +1,16 @@
-"use strict";
-exports.__esModule = true;
-exports.GraphVertex = void 0;
 /**
  *
  *
  * @export
  * @class GraphVertex
  */
-var GraphVertex = /** @class */ (function () {
+export class GraphVertex {
     /**
      *Creates an instance of GraphVertex.
      * @param {string} key
      * @memberof GraphVertex
      */
-    function GraphVertex(key) {
+    constructor(key) {
         this.key = key;
         this.edges = [];
     }
@@ -21,48 +18,44 @@ var GraphVertex = /** @class */ (function () {
      * @return {string}
      * @memberof GraphVertex
      */
-    GraphVertex.prototype.getKey = function () {
+    getKey() {
         return this.key;
-    };
+    }
     /**
      * @param {GraphEdge} edge
      * @return {GraphVertex}
      * @memberof GraphVertex
      */
-    GraphVertex.prototype.addEdge = function (edge) {
+    addEdge(edge) {
         this.edges.push(edge);
         return this;
-    };
+    }
     /**
      * @param {GraphVertex} vertex
      * @return {GraphEdge}
      * @memberof GraphVertex
      */
-    GraphVertex.prototype.findEdge = function (vertex) {
-        var _a;
-        var edge = this.edges.filter(function (edge) {
+    findEdge(vertex) {
+        const edge = this.edges.filter((edge) => {
             return edge.startVertex === vertex || edge.endVertex === vertex;
         });
-        return (_a = edge[0]) !== null && _a !== void 0 ? _a : null;
-    };
+        return edge[0] ?? null;
+    }
     /**
      * @return {GraphVertex[]}
      * @memberof GraphVertex
      */
-    GraphVertex.prototype.getNeighbors = function () {
-        var _this = this;
-        var edges = this.edges;
-        return edges.map(function (edge) {
-            return edge.startVertex === _this ? edge.endVertex : edge.startVertex;
+    getNeighbors() {
+        const edges = this.edges;
+        return edges.map((edge) => {
+            return edge.startVertex === this ? edge.endVertex : edge.startVertex;
         });
-    };
+    }
     /**
      * @return {GraphEdge[]}
      * @memberof GraphVertex
      */
-    GraphVertex.prototype.getEdges = function () {
+    getEdges() {
         return this.edges;
-    };
-    return GraphVertex;
-}());
-exports.GraphVertex = GraphVertex;
+    }
+}
