d6dee78fbca013027c21ba31e13aad7e74bac99e
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-11-23 21:57:03 +0000

    graph config

--- data-structures/graphConfig.js
+++ data-structures/graphConfig.js
@@ -1,24 +1,18 @@
-"use strict";
-exports.__esModule = true;
-exports.GraphConfig = void 0;
 /**
  *
  *
  * @export
  * @class GraphConfig
  */
-var GraphConfig = /** @class */ (function () {
+export class GraphConfig {
     /**
      *Creates an instance of GraphConfig.
      * @param {*} {enterVertex, leaveVertex, allowEnterVertex}
      * @memberof GraphConfig
      */
-    function GraphConfig(_a) {
-        var enterVertex = _a.enterVertex, leaveVertex = _a.leaveVertex, allowEnterVertex = _a.allowEnterVertex;
+    constructor({ enterVertex, leaveVertex, allowEnterVertex }) {
         this.enterVertex = enterVertex;
         this.leaveVertex = leaveVertex;
         this.allowEnterVertex = allowEnterVertex;
     }
-    return GraphConfig;
-}());
-exports.GraphConfig = GraphConfig;
+}
