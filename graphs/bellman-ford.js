f6acedaea352185cea1c6aeb1d4b6f226473b950
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-11-23 21:57:22 +0000

    bellman

--- graphs/bellman-ford.js
+++ graphs/bellman-ford.js
@@ -1,6 +1,3 @@
-"use strict";
-exports.__esModule = true;
-exports.bellmanFord = void 0;
 /**
  *
  *
@@ -12,23 +9,21 @@ exports.bellmanFord = void 0;
  *   prev: Map<string, string>
  * }}
  */
-function bellmanFord(graph, source) {
-    var dist = new Map();
-    var prev = new Map();
-    var vertices = graph.getVertices();
-    for (var _i = 0, vertices_1 = vertices; _i < vertices_1.length; _i++) {
-        var vertex = vertices_1[_i];
+export function bellmanFord(graph, source) {
+    const dist = new Map();
+    const prev = new Map();
+    const vertices = graph.getVertices();
+    for (const vertex of vertices) {
         dist[vertex.getKey()] = Infinity;
         prev[vertex.getKey()] = undefined;
     }
     dist[source.getKey()] = 0;
-    var edges = graph.getEdges();
-    for (var i = 0, j = vertices.length - 1; i < j; i++) {
-        for (var _a = 0, edges_1 = edges; _a < edges_1.length; _a++) {
-            var edge = edges_1[_a];
-            var u = edge.startVertex.getKey();
-            var v = edge.endVertex.getKey();
-            var weight = edge.weight;
+    const edges = graph.getEdges();
+    for (let i = 0, j = vertices.length - 1; i < j; i++) {
+        for (const edge of edges) {
+            const u = edge.startVertex.getKey();
+            const v = edge.endVertex.getKey();
+            const weight = edge.weight;
             if (dist[u] + weight < dist[v]) {
                 // a new s.p. has been found
                 dist[v] = dist[u] + weight;
@@ -37,18 +32,16 @@ function bellmanFord(graph, source) {
         }
     }
     // check for negative weight cycles
-    for (var _b = 0, edges_2 = edges; _b < edges_2.length; _b++) {
-        var edge = edges_2[_b];
-        var u = edge.startVertex.getKey();
-        var v = edge.endVertex.getKey();
-        var weight = edge.weight;
+    for (const edge of edges) {
+        const u = edge.startVertex.getKey();
+        const v = edge.endVertex.getKey();
+        const weight = edge.weight;
         if (dist[u] + weight < dist[v]) {
             throw new Error('Graph contains a negative-weight cycle');
         }
     }
     return {
-        dist: dist,
-        prev: prev
+        dist,
+        prev,
     };
 }
-exports.bellmanFord = bellmanFord;
