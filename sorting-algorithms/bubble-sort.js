47f4475a2ffd453f3d6a8ebada73c69e83d1dda2
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-12-06 23:34:01 +0000

    Bubble sort

--- sorting-algorithms/bubble-sort.js
+++ sorting-algorithms/bubble-sort.js
@@ -1,10 +1,10 @@
-var bubbleSort = function (arr) {
-    var _a;
-    var length = arr.length;
-    for (var i = 0; i < length; i++) {
-        for (var j = 0; j < length; j++) {
+//Bubble sort
+const bubbleSort = (arr) => {
+    const length = arr.length;
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length; j++) {
             if (arr[j] > arr[j + 1]) {
-                _a = [arr[j + 1], arr[j]], arr[j] = _a[0], arr[j + 1] = _a[1];
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
         }
     }
