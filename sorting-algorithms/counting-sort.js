0209d54e3b9413be93c2862faba2bacc1d0b7a1b
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-12-12 20:20:04 +0000

    Counting sort

--- sorting-algorithms/counting-sort.js
+++ sorting-algorithms/counting-sort.js
@@ -1,6 +1,3 @@
-"use strict";
-exports.__esModule = true;
-exports.sort = void 0;
 /**
  * Sort an array with a counting sort algorithm
  *
@@ -9,8 +6,7 @@ exports.sort = void 0;
  * @param {number} k
  * @return {(number[] | null)}
  */
-function sort(array, k) {
-    if (k === void 0) { k = 255; }
+export function sort(array, k = 255) {
     // Result is null for a nullable or undefined array
     if (!array) {
         return null;
@@ -21,27 +17,24 @@ function sort(array, k) {
     // Count a number of occurences:
     //  * array: [1, 2, 1, 4] =>
     //  * count: [2, 1, 0, 1]
-    var count = new Array(k).fill(0);
-    for (var _i = 0, array_1 = array; _i < array_1.length; _i++) {
-        var el = array_1[_i];
+    const count = new Array(k).fill(0);
+    for (const el of array) {
         count[el] = (count[el] + 1) || 1;
     }
     // Summarize a current element with a previous:
     //  * count: [2, 1, 0, 1] =>
     //  * count: [2, 3, 3, 4]
-    for (var i = 1; i < count.length; i++) {
+    for (let i = 1; i < count.length; i++) {
         count[i] += count[i - 1];
     }
     // Insert input numbers to appropriate positions
     //  * array: [1, 2, 1, 4]
     //  * count: [2, 3, 3, 4]
     //  * output: [0, 1, 0, 0] => [0, 1, 2, 0] => [1, 1, 2, 0] => [1, 1, 2, 4]
-    var output = new Array(array.length).fill(0);
-    for (var _a = 0, array_2 = array; _a < array_2.length; _a++) {
-        var el = array_2[_a];
+    const output = new Array(array.length).fill(0);
+    for (const el of array) {
         output[count[el] - 1] = el;
         count[el] -= 1;
     }
     return output;
 }
-exports.sort = sort;
