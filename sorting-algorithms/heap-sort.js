952752b0ca104362051b6d881a0faa12a4b1ea1c
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-12-12 20:20:36 +0000

    Heap sort

--- sorting-algorithms/heap-sort.js
+++ sorting-algorithms/heap-sort.js
@@ -1,22 +1,20 @@
-var HeapSort = /** @class */ (function () {
-    function HeapSort() {
-    }
-    HeapSort.prototype.sort = function (arr) {
-        var size = arr.length;
-        for (var i = Math.floor(size / 2) - 1; i >= 0; i--) {
+class HeapSort {
+    sort(arr) {
+        const size = arr.length;
+        for (let i = Math.floor(size / 2) - 1; i >= 0; i--) {
             this.heapify(arr, size, i);
         }
-        var j = size - 1;
+        let j = size - 1;
         while (j >= 1) {
             this.swap(arr, 0, j);
             this.heapify(arr, j, 0);
             j--;
         }
-    };
-    HeapSort.prototype.heapify = function (arr, size, i) {
-        var largest = i;
-        var leftLeaf = 2 * i + 1;
-        var rightLeaf = 2 * i + 2;
+    }
+    heapify(arr, size, i) {
+        let largest = i;
+        let leftLeaf = 2 * i + 1;
+        let rightLeaf = 2 * i + 2;
         // If the left child is larger than the current largest.
         if (leftLeaf < size && arr[leftLeaf] > arr[largest]) {
             largest = leftLeaf;
@@ -32,11 +30,10 @@ var HeapSort = /** @class */ (function () {
             // Heapify the sub-tree. 
             this.heapify(arr, size, largest);
         }
-    };
-    HeapSort.prototype.swap = function (arr, a, b) {
-        var tmp = arr[a];
+    }
+    swap(arr, a, b) {
+        const tmp = arr[a];
         arr[a] = arr[b];
         arr[b] = tmp;
-    };
-    return HeapSort;
-}());
+    }
+}
