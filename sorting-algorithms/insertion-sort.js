427926f61b7b6b4da4a6ae96a8c1ec89623a41ac
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-12-13 18:11:01 +0000

    Insertion

--- sorting-algorithms/insertion-sort.js
+++ sorting-algorithms/insertion-sort.js
@@ -1,13 +1,12 @@
-var insertionSort = function (arr) {
-    var _a;
+const insertionSort = (arr) => {
     //Start from the second element.
-    for (var i = 1; i < arr.length; i++) {
+    for (let i = 1; i < arr.length; i++) {
         //Go through the elements behind it.
-        for (var j = i - 1; j > -1; j--) {
+        for (let j = i - 1; j > -1; j--) {
             //value comparison using ascending order.
             if (arr[j + 1] < arr[j]) {
                 //swap
-                _a = [arr[j], arr[j + 1]], arr[j + 1] = _a[0], arr[j] = _a[1];
+                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
             }
         }
     }
