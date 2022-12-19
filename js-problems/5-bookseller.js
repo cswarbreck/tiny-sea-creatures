8a86bf597cd4caadc51a09e369e3ddc9ea038a00
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-12-19 23:35:53 +0000

    Adds tests

--- js-problems/5-bookseller.js
+++ js-problems/5-bookseller.js
@@ -56,3 +56,7 @@ function stockList(listOfArt, listOfCat){
   
 //   console.log(listOfArt, listOfCat)
 }  
+
+// ART = ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"]
+// CAT = ["A", "B", "C", "D"]
+// res = "(A : 0) - (B : 1290) - (C : 515) - (D : 600)"
