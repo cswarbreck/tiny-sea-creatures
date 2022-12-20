7233d67442c43a514fd9c57ceb0591d5cf645be8
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-12-20 19:14:43 +0000

    Selection sort

--- sorting-algorithms/selection-sort.js
+++ sorting-algorithms/selection-sort.js
@@ -1,18 +1,17 @@
-var selectionSort = function (arr) {
-    var _a;
-    var len = arr.length;
-    var minInd = -1;
-    for (var i = 0; i < (len - 1); i++) {
+const selectionSort = (arr) => {
+    const len = arr.length;
+    let minInd = -1;
+    for (let i = 0; i < (len - 1); i++) {
         minInd = i;
-        for (var j = (i + 1); j < len; j++) {
+        for (let j = (i + 1); j < len; j++) {
             if (arr[j] < arr[minInd]) {
                 minInd = j;
             }
         }
         if (minInd !== i) {
-            _a = [arr[minInd], arr[i]], arr[i] = _a[0], arr[minInd] = _a[1];
+            [arr[i], arr[minInd]] = [arr[minInd], arr[i]];
         }
     }
     return arr;
 };
-var result = selectionSort([64, 25, 12, 22, 11]);
+const result = selectionSort([64, 25, 12, 22, 11]);
