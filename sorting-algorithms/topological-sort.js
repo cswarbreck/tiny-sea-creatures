0e5340317c27743a9715782b02383c05928eb8b2
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-12-20 19:14:49 +0000

    Topological sort

--- sorting-algorithms/topological-sort.js
+++ sorting-algorithms/topological-sort.js
@@ -1,7 +1,4 @@
-"use strict";
-exports.__esModule = true;
-exports.topological = void 0;
-var depthFirstSearch_1 = require("../data-structures/depthFirstSearch");
+import { depthFirstSearch } from '../data-structures/depthFirstSearch';
 /**
  *
  *
@@ -10,30 +7,29 @@ var depthFirstSearch_1 = require("../data-structures/depthFirstSearch");
  * @param {GraphConfig} config
  * @return {GraphVertex[]}
  */
-function topological(graph, config) {
-    if (config === void 0) { config = null; }
+export function topological(graph, config = null) {
     if (!graph) {
         return;
     }
     // A set of unvisited vertices
-    var unvisited = {};
-    graph.getVertices().forEach(function (vertex) {
+    const unvisited = {};
+    graph.getVertices().forEach((vertex) => {
         unvisited[vertex.getKey()] = vertex;
     });
     // A set of visited vertices
-    var visited = {};
+    const visited = {};
     // An array, which behaves like a stack (to hold the order)
-    var stack = [];
+    const stack = [];
     config = config || {};
     // Visit only unvisited vertices
     config.allowEnterVertex =
         config.allowEnterVertex ||
-            (function (vertex) {
+            ((vertex) => {
                 return !visited[vertex.getKey()];
             });
     config.enterVertex =
         config.enterVertex ||
-            (function (vertex) {
+            ((vertex) => {
                 // Remove a vertex from the unvisited set
                 delete unvisited[vertex.getKey()];
                 // Mark a vertex as visited
@@ -43,15 +39,14 @@ function topological(graph, config) {
      * when all of the vertex neighbors have been visited */
     config.leaveVertex =
         config.leaveVertex ||
-            (function (vertex) {
+            ((vertex) => {
                 // TODO: Replace with an actual stack implementation
                 stack.push(vertex);
             });
     while (Object.keys(unvisited).length) {
-        var startVertexKey = Object.keys(unvisited)[0];
-        var startVertex = unvisited[startVertexKey];
-        (0, depthFirstSearch_1.depthFirstSearch)(graph, startVertex, config);
+        const startVertexKey = Object.keys(unvisited)[0];
+        const startVertex = unvisited[startVertexKey];
+        depthFirstSearch(graph, startVertex, config);
     }
     return stack.reverse();
 }
-exports.topological = topological;
