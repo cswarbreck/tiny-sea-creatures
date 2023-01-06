f9313c20c6804523841a42dc84c6b97efeb2e693
Cosmo Swarbreck <c.swarbreck@gmail.com>
2023-01-06 18:56:34 +0000

    Refactor

--- 5-weight-for-weight.ts
+++ 5-weight-for-weight.ts
@@ -31,40 +31,40 @@ export function orderWeight(str: any): string {
 
 //Inelegant Solution
 
-// export function orderWeight(str: string): any {
-//   let obj: {
-//     key: string,
-//     value: string
-//   }
+export function orderWeight(str: string): any {
+  let obj: {
+    key: string,
+    value: string
+  }
 
-//   if (str) {
-//     str.split(" ").forEach((el: string) => {
-//       let total: number = el
-//         .split("")
-//         .map(Number)
-//         .reduce((a: number, b: number) => {
-//           return a + b;
-//         });
-//       !obj[total] ? (obj[total] = [el]) : obj[total].push(el);
-//     });
-//   }
+  if (str) {
+    str.split(" ").forEach((el: string) => {
+      let total: number = el
+        .split("")
+        .map(Number)
+        .reduce((a: number, b: number) => {
+          return a + b;
+        });
+      !obj[total] ? (obj[total] = [el]) : obj[total].push(el);
+    });
+  }
 
-//   let orderedObj: {} = {};
-//   Object.keys(obj)
-//     .sort()
-//     .forEach((key: string) => {
-//       orderedObj[key] = obj[key];
-//     });
+  let orderedObj: {} = {};
+  Object.keys(obj)
+    .sort()
+    .forEach((key: string) => {
+      orderedObj[key] = obj[key];
+    });
 
-//   let finalArr: string[] = [];
-//   Object.keys(orderedObj).forEach((key: string) => {
-//     orderedObj[key].length > 1
-//       ? (finalArr = [...finalArr, ...orderedObj[key].sort()])
-//       : finalArr.push(orderedObj[key][0]);
-//   });
+  let finalArr: string[] = [];
+  Object.keys(orderedObj).forEach((key: string) => {
+    orderedObj[key].length > 1
+      ? (finalArr = [...finalArr, ...orderedObj[key].sort()])
+      : finalArr.push(orderedObj[key][0]);
+  });
 
-//   return finalArr.join(" ");
-// }
+  return finalArr.join(" ");
+}
 
 
 
